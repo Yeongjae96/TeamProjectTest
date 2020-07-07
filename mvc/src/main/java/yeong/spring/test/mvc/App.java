a4c18ec3cdd04238451eaccdbc32f61d27fb4335
Yeong <yeongjae1996@gmail.com>
2020-07-08 00:36:16 +0900

    init project complete!

---
+++ mvc/src/main/java/yeong/spring/test/mvc/App.java
@@ -0,0 +1,15 @@
+package yeong.spring.test.mvc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("왤컴 투 초기설정");
+    }
+}
